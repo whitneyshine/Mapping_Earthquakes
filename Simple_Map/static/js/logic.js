16fa95ecfe39eb8fdbbe863281e1a5d732f0735d
whitneyshine <wshine79@sbcglobal.net>
2021-05-21 11:13:49 -0500

    Mapping_GeoJSON_LineStrings

--- Simple_Map/static/js/logic.js
+++ Simple_Map/static/js/logic.js
@@ -28,18 +28,24 @@ let map = L.map('mapid', {
 // Pass our map layers into our layers control and add the layers control to the map.
 L.control.layers(baseMaps).addTo(map);
 
-// Accessing the airport GeoJSON URL
-let airportData = "https://raw.githubusercontent.com/whitneyshine/Mapping_Earthquakes/main/majorAirports.json";
+// Accessing the Toronto airline routes GeoJSON URL.
+let torontoData = "https://raw.githubusercontent.com/whitneyshine/Mapping_Earthquakes/main/torontoRoutes.json";
+
+// Create a style for the lines.
+let myStyle = {
+  color: "#ffffa1",
+  weight: 2
+}
 
 // Grabbing our GeoJSON data.
-d3.json(airportData).then(function(data) {
+d3.json(torontoData).then(function(data) {
   console.log(data);
 // Creating a GeoJSON layer with the retrieved data.
-L.geoJson(data).addTo(map);
+L.geoJson(data, {
+	style: myStyle, 
+	onEachFeature: function(feature, layer) {
+		layer.bindPopup("<h3> Airline: " + feature.properties.airline +"</h3> <hr> <h3> Destination: " + feature.properties.dst + "</h3>");
+	}
+}).addTo(map);
 });
 
-
-
-
-// Then we add our 'graymap' tile layer to the map.
-streets.addTo(map);
